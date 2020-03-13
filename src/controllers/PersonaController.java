1bb40ad6c6a8def760120d279d3a790aa38ab31d
Edgard <edgard@barquero.me>
2020-03-12 22:03:59 -0600

    Se agrega el CRUD

--- src/controllers/PersonaController.java
+++ src/controllers/PersonaController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import models.Persona;
 
 public class PersonaController {
@@ -58,18 +59,86 @@ public class PersonaController {
             this.personas.add(p);
             this.tblPersonas.setItems(personas);
 
-            this.txtNombre.setText("");
-            this.txtApellido.setText("");
-            this.txtEdad.setText("");
+            this.limpiar();
 
         }else{
-            System.out.println(personas);
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setHeaderText(null);
-            alert.setTitle(null);
-            alert.setContentText("Ya existe la persona");
-            alert.showAndWait();
+            this.advertencia("Ya existe la persona");
         }
 
     }
+
+    public void seleccionar(MouseEvent mouseEvent) {
+
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p != null){
+            this.txtNombre.setText(p.getNombre());
+            this.txtApellido.setText(p.getApellido());
+            this.txtEdad.setText(String.valueOf(p.getEdad()));
+        }
+
+    }
+
+    public void modificar(ActionEvent actionEvent) {
+
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p == null){
+            this.advertencia("Debes seleccionar una persona");
+        }else{
+
+            String nombre = this.txtNombre.getText();
+            String apellido = this.txtApellido.getText();
+            Integer edad = Integer.valueOf(this.txtEdad.getText());
+
+            Persona aux = new Persona(nombre, apellido, edad);
+
+            if(!this.personas.contains(aux)){
+
+                p.setNombre(aux.getNombre());
+                p.setApellido(aux.getApellido());
+                p.setEdad(aux.getEdad());
+
+                this.tblPersonas.refresh();
+
+            }
+
+        }
+
+
+    }
+
+    private void advertencia(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setHeaderText((String)null);
+        alert.setTitle(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
+    }
+
+    public void eliminar(ActionEvent actionEvent) {
+
+        Persona p = this.tblPersonas.getSelectionModel().getSelectedItem();
+
+        if(p == null){
+            this.advertencia("Debes seleccionar una persona para eliminar");
+        }else{
+
+            this.personas.remove(p);
+            this.tblPersonas.refresh();
+
+            this.limpiar();
+
+        }
+
+    }
+
+    public void limpiar(){
+
+        this.txtNombre.setText("");
+        this.txtApellido.setText("");
+        this.txtEdad.setText("");
+
+    }
+
 }
